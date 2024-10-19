057e8564b9fff8dd89c97a129f301c3885f39499
Samiul Alim <xo.ot2001@gmail.com>
2024-10-19 23:59:52 +0600

    Feat: Added Form Schema, Migrations and Types

---
+++ prisma/migrations/20241019154032_forms_table/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Form" (
+    "id" TEXT NOT NULL,
+    "templateId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "answers" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Form_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_id_key" ON "Form"("id");
+
+-- CreateIndex
+CREATE INDEX "Form_userId_idx" ON "Form"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Form" ADD CONSTRAINT "Form_templateId_fkey" FOREIGN KEY ("templateId") REFERENCES "Template"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Form" ADD CONSTRAINT "Form_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ types/Form.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+const FormAnswersSchema = z.object({
+  questionId: z.string(),
+  question: z.string(),
+  questionType: z.enum(["short", "paragraph", "multipleChoice"]),
+  answer: z.string(),
+});
+
+const FormSchema = z.object({
+  id: z.string().uuid().optional(),
+  userId: z.string(),
+  templateId: z.string(),
+  answers: z.array(FormAnswersSchema),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
+});
+
+type FormAnswers = z.infer<typeof FormAnswersSchema>;
+type Form = z.infer<typeof FormSchema>;
+
+export type { FormAnswers, Form };
+export { FormSchema, FormAnswersSchema };
